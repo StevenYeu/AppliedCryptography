5db6676040d13eefc17a5d9a783e010a4521a0f5
stevenyeu <yeusteven@gmail.com>
2016-06-03 01:44:51 -0700

    Fixed Bug

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,11 @@
-def convert_to_bits(n,pad):
+def convert_to_bits(n, pad):
     result = []
     while n > 0:
         if n % 2 == 0:
             result = [0] + result
         else:
             result = [1] + result
-    while len(result) < n :
-        result =  [0] + result
+        n = n // 2
+    while len(result) < pad:
+        result = [0] + result
     return result
